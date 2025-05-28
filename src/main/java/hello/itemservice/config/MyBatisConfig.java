16782d8a5cfd13af3f876a5050ccdf5f6d57022d
jurio5 <sia5492@naver.com>
2025-05-28 23:58:15 +0900

    [feat] Mybatis 설정 및 DB 빈 등록 후 연결

---
+++ src/main/java/hello/itemservice/config/MyBatisConfig.java
@@ -0,0 +1,30 @@
+package hello.itemservice.config;
+
+import hello.itemservice.repository.ItemRepository;
+import hello.itemservice.repository.mybatis.ItemMapper;
+import hello.itemservice.repository.mybatis.MyBatisItemRepository;
+import hello.itemservice.service.ItemService;
+import hello.itemservice.service.ItemServiceV1;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class MyBatisConfig {
+    
+    // DataSource 와 TransactionManager 의 경우는 MyBatis 모듈에서 자동으로 읽어서 연결
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    private final ItemMapper itemMapper;
+
+    @Bean
+    public ItemService itemService() {
+        return new ItemServiceV1(itemRepository());
+    }
+
+    @Bean
+    public ItemRepository itemRepository() {
+        return new MyBatisItemRepository(itemMapper);
+    }
+
+}
